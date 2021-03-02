1a0a98296661b20fbddaee818f94c5e3abac9098
BessonMikael <besson.mikael04@gmail.com>
2021-03-02 16:39:16 +0100

    exo 197

--- index.php
+++ index.php
@@ -8,6 +8,69 @@
 </head>
 <body>
 <?php
+require './Classes/DB.php';
+$conn = DB::getInstance();
+
+$search = $conn->prepare("SELECT id, lastName, firstName FROM clients");
+$result = $search->execute();
+if($result){
+        echo "liste des utilisateurs :<br>";
+    foreach($search->fetchAll() AS $clients) {
+        echo "<span>";
+        echo $clients['id'] . " " . $clients['lastName'] . " " . $clients['firstName'] . "<br>";
+        echo "</span>";
+    }
+}
+echo "<br>";
+
+$search = $conn->prepare("SELECT type FROM showtypes");
+$result = $search->execute();
+if($result){
+    echo "liste des spectacles :<br>";
+    foreach($search->fetchAll() AS $showtypes) {
+        echo "<span>";
+        echo $showtypes['type'] . "<br>";
+        echo "</span>";
+    }
+}
+echo "<br>";
+
+$search = $conn->prepare("SELECT * FROM clients LIMIT 20");
+$result = $search->execute();
+if($result){
+    echo "liste des 20 premiers clients :<br>";
+    foreach($search->fetchAll() AS $clients) {
+        echo "<span>";
+        echo $clients['id'] ." ". $clients['lastName'] ." ". $clients['firstName'] ."<br>";
+        echo "</span>";
+    }
+}
+echo "<br>";
+
+$search = $conn->prepare("SELECT * FROM clients WHERE card=1");
+$result = $search->execute();
+if($result){
+    echo "liste des clients possédant une carte de fidélité :<br>";
+    foreach($search->fetchAll() AS $clients) {
+        echo "<span>";
+        echo  $clients['lastName'] ." ". $clients['firstName'] ."<br>";
+        echo "</span>";
+    }
+}
+echo "<br>";
+
+$search = $conn->prepare("SELECT * FROM clients WHERE lastName LIKE 'M%' ORDER BY firstName ASC");
+$result = $search->execute();
+if($result){
+    echo "liste des clients dont le nom commence par M :<br>";
+    foreach($search->fetchAll() AS $clients) {
+        echo "<span>";
+        echo  $clients['firstName'] ."<br>";
+        echo $clients['lastName'] ."<br>";
+        echo "</span>";
+    }
+}
+echo "<br>";
 
 ?>
 
